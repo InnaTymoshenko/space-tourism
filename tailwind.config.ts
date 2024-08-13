0032593915536c728a3eb1cc4aaefa0bbef9eea0
InnaTymoshenko <innatymosh@gmail.com>
2024-08-13 18:04:37 +0300

    space-tourism init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}'
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				desktop: "url('/images/background-home-desktop.jpg')",
+				tablet: "url('/images/background-home-tablet.jpg')",
+				mobile: "url('/images/background-home-mobile.jpg')",
+				'dest-desktop': "url('/images/background-destination-desktop.jpg')",
+				'dest-tablet': "url('/images/background-destination-tablet.jpg')",
+				'dest-mobile': "url('/images/background-destination-mobile.jpg')",
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+			}
+		}
+	},
+	plugins: []
+}
+export default config
